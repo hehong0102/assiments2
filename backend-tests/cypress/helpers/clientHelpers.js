521c2a178dcb2faa78c64a129f462b59f88cdade
honghe <hehong0102@hotmail.com>
2020-11-17 14:46:26 +0100

    assigment2

---
+++ backend-tests/cypress/helpers/clientHelpers.js
@@ -0,0 +1,189 @@
+//import faker from 'faker'
+const faker = require('faker')
+
+const ENDPOINT_GET_CLIENTS = 'http://localhost:3000/api/clients'
+const ENDPOINT_POST_CLIENT = 'http://localhost:3000/api/client/new'
+const ENDPOINT_GET_CLIENT = 'http://localhost:3000/api/client/'
+const fakeName = faker.name.firstName()
+const fakeEmail = faker.internet.email()
+const fakePhone = faker.phone.phoneNumber()
+
+function createRandomClientPayload(){
+    
+    const payload = {
+        "name":fakeName,
+        "email":fakeEmail,
+        "telephone":fakePhone
+    }
+
+    return payload
+}
+
+function getRequestAllClientsWithAssertion(cy,name, email, telephone){
+    // GET request to fetch all clients
+    cy.request({
+        method: "GET",
+        url: ENDPOINT_GET_CLIENTS,
+        headers:{
+            'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+            'Content-Type': 'application/json'
+        },
+    }).then((response =>{
+        const responseAsString = JSON.stringify(response)
+        expect(responseAsString).to.have.string(name)
+        expect(responseAsString).to.have.string(email)
+        expect(responseAsString).to.have.string(telephone)
+    }))
+}
+
+function getAllClientsRequest(cy){
+    cy.authenticateSession().then((response =>{
+        cy.request({
+            method: "GET",
+            url: ENDPOINT_GET_CLIENTS,
+            headers:{
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+        }).then((response =>{
+            const responseAsString = JSON.stringify(response)
+            cy.log(responseAsString)
+           
+        }))
+    }))
+}
+
+function deleteRequestsAfterGet(cy){
+    // GET request to fetch all clients
+    cy.request({
+        method: "GET",
+        url: ENDPOINT_GET_CLIENTS,
+        headers:{
+            'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+            'Content-Type': 'application/json'
+        },
+    }).then((response =>{
+        let lastId=response.body[response.body.length -1].id
+        cy.request({
+            method:"DELETE",
+            url:ENDPOINT_GET_CLIENT+lastId,
+            headers:{
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+
+        })
+    }))
+}
+
+function editClientRequestAfterget(cy){
+    
+    cy.request({
+        method: "GET",
+        url: ENDPOINT_GET_CLIENTS,
+        headers:{
+            'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+            'Content-Type': 'application/json'
+        },
+    }).then((response =>{
+        let lastIdForEdit=response.body[response.body.length -1].id
+        cy.request({
+            method:"put",
+            url:ENDPOINT_GET_CLIENT+lastIdForEdit,
+            headers:{
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+            body:{
+                "id":lastIdForEdit,
+                "name":fakeName,
+                "email":fakeEmail,
+                "telephone":fakePhone
+                
+            }
+
+        }).then((response =>{               
+            const responseAsString = JSON.stringify(response)
+            expect(responseAsString).to.have.string(fakeName)
+         }))
+ 
+         
+     }))
+    }
+
+
+function createClientRequest(cy){
+    cy.authenticateSession().then((response =>{
+        let fakeClientPayload = createRandomClientPayload() 
+        
+        // post request to create a client
+        cy.request({
+            method: "POST",
+            url: ENDPOINT_POST_CLIENT,
+            headers:{
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+            body:fakeClientPayload 
+        }).then((response =>{               
+           const responseAsString = JSON.stringify(response)
+           expect(responseAsString).to.have.string(fakeClientPayload.name)
+        }))
+
+        getRequestAllClientsWithAssertion(cy,fakeClientPayload.name, fakeClientPayload.email, fakeClientPayload.telephone)
+    }))
+}
+
+
+function createClientRequestAndDelete(cy){
+    cy.authenticateSession().then((response =>{
+        let fakeClientPayload = createRandomClientPayload() 
+        
+        // post request to create a client
+        cy.request({
+            method: "POST",
+            url: ENDPOINT_POST_CLIENT,
+            headers:{
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+            body:fakeClientPayload 
+        }).then((response =>{               
+           const responseAsString = JSON.stringify(response)
+           expect(responseAsString).to.have.string(fakeClientPayload.name)
+        }))
+
+        deleteRequestsAfterGet(cy)
+    }))
+}function createClientRequestAndEdit(cy){
+    cy.authenticateSession().then((response =>{
+        let fakeClientPayload = createRandomClientPayload() 
+        
+        
+        cy.request({
+            method: "POST",
+            url: ENDPOINT_POST_CLIENT,
+            headers:{
+                'X-User-Auth': JSON.stringify(Cypress.env().loginToken),
+                'Content-Type': 'application/json'
+            },
+            body:fakeClientPayload 
+        }).then((response =>{               
+           const responseAsString = JSON.stringify(response)
+           expect(responseAsString).to.have.string(fakeClientPayload.name)
+        }))
+
+        editClientRequestAfterget(cy)
+    }))
+}
+
+
+
+module.exports = {
+    createRandomClientPayload, 
+    createClientRequest, 
+    getAllClientsRequest,
+    createClientRequestAndDelete,
+    editClientRequestAfterget,
+    createClientRequestAndEdit,
+}
